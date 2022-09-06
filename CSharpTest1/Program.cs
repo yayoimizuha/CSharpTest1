0e4888173937c76b0e1931d24b82dc3d3521c6d4
pokop <pokop@DESKTOP-H2LB90G>
2022-09-07 00:01:35 +0900

    プロジェクト ファイルを追加します。

---
+++ CSharpTest1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Func1("https://example.com/").Result);
+        }
+
+        static async Task<string> Func1(string args)
+        {
+            HttpClient client = new();
+            string requestBody = "";
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(args);
+                response.EnsureSuccessStatusCode();
+                requestBody = await response.Content.ReadAsStringAsync();
+            }catch(HttpRequestException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return requestBody;
+        }
+    }
+}
